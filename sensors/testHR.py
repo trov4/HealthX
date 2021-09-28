41e686fad9e74f7dd4f6598783743468097500aa
Trov4 <tstanca@students.kennesaw.edu>
2021-09-28 14:48:19 +0100

    This is my commit message

--- sensors/testHR.py
+++ sensors/testHR.py
@@ -8,6 +8,7 @@ import numpy as np
 m = max30102.MAX30102()
 
 dataHR=[]
+hr_list = []
 time.sleep(5)   # Wait 5 seconds to place finger on the sensor.
 trimmed_sum = 0
 sum = 0
@@ -16,22 +17,25 @@ count = 0
 array=0
 count1 = 0
 #n = len(hr)
-for i in range(5):
+#for i in range(5):
 #init
 
 # len(dataHR)
-#while len (dataHR) < 5:
+while len(hr_list) < 5:
 
     red, ir = m.read_sequential()
 
-    dataHR[i*100:(i+1)*100] = red    # For raw data capturing. Not required!
+    #dataHR[i*100:(i+1)*100] = red    # For raw data capturing. Not required!
     hr, hr_valid, spo2, spo2_valid = hrcalc.calc_hr_and_spo2(ir[:100], red[:100])  # Calculating heart rate and SpO2 values from raw data.
-    if dataHR < 0: 
+    # hr checks, also check if over maximum
+    if hr < 0: 
 	continue
-    dataHR.append(hr)
+    hr_list.append(hr)
 
-    dataHR.sort()
-    dataHR = dataHR[1:-1]
+    # spo2 checks, min, max, maximum samples
+
+hr_list.sort()
+hr_list = hr_list[1:-1]
 
 #    def tmeanHR(hr, alpha):
 #    a = arr.array('d', [hr])
@@ -68,18 +72,24 @@ for i in range(5):
 #trimmed_avg = sum(trimmed_list)/len(trimmed_list)
 	#return float(trimmed_avg)
 #	print "Average Heart Rate: ", trimmed_avg
-		
-    if dataHR > 0:
-	count = count + hr
-    	sum = sum + hr
-    	avg = sum/3
+print "hr list: ", hr_list
+summer = 0
+for i in hr_list:
+    summer = summer + i
+l = len(hr_list)
+
+avg = summer/l
+#    if dataHR > 0:
+#	count = count + hr
+ #   	sum = sum + hr
+  #  	avg = sum/3
 #	stats.trim_mean(hr,0.1)
-    if spo2 > 0:
-	count1 = count1 + spo2
-	sum1 = sum1 + spo2
-	avg1 = sum1/5
+   # if spo2 > 0:
+#	count1 = count1 + spo2
+#	sum1 = sum1 + spo2
+#	avg1 = sum1/5
 #	stats.trim_mean(spo2,0.1)
-    print "Heart Rate: ", hr, ", Blood Oxygen: ", spo2
+print "Heart Rate: ", hr, ", Blood Oxygen: ", spo2
 #print trimmed_avg
 print "Average Heart Rate: ", avg
 #print "Average Blood Oxygen: ", avg1
